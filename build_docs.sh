71637c3c360b9c1c43189a254067586b5fc60213
Everything Security <everything-security@l8e.tech>
2025-08-18 23:55:11 -0700

    add netlify docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# Install pdoc and generate the documentation
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Install pdoc
 pip install pdoc
+
+# Generate the documentation from your source code
 pdoc src/l8e_beam -o docs/
